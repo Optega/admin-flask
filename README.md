7da1ddf2ea65d434e78356c9586193a87c9e408a
Optega <optega@gmail.com>
2024-11-11 16:42:40 +0200

    Add login functionality and environment configuration
    - Created a new .env example file for environment variables.
    - Updated requirements.txt to include python-dotenv for loading environment variables.
    - Modified .gitignore to exclude .env and .vscode directories.
    - Refactored CSS styles and added Tailwind CSS utility classes.
    - Implemented login and logout routes with session management in app.py.
    - Added a login template and updated the index template to include favicon.
    - Enhanced README with setup instructions for .env and recipients.json.

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # Change the data in the file to Flask
 
-## Before use
+## Setup
 
-You need to create a file ```admin/recipients.json``` with the data
+### .env
+
+1. Create `.env` from `.evn example`;
+2. Update the data in `.env`
+
+A secret key should be as random as possible. Your operating system has ways to generate pretty random data based on a cryptographic random generator. Use the following command to quickly generate a value for `FLASK_SECRET_KEY`:
+
+```bash
+python -c 'import secrets; print(secrets.token_hex())'
+```
+
+### recipients.json
+
+You need to create a file `admin/recipients.json` with the data template
 
 ```json
 [
@@ -29,10 +42,10 @@ You need to create a file ```admin/recipients.json``` with the data
 ]
 ```
 
-The program is created to edit the ```recipients.json``` file.
-In docker, the ```recipients.json``` file is used by volume, it can be used in other code.
+The program is created to edit the `recipients.json` file.
+In docker, the `recipients.json` file is used by volume, it can be used in other code.
 
-I am using code as an admin to change message recipients. I have a script that sends messages to recipients in the ```recipients.json``` file
+I am using code as an admin to change message recipients. I have a script that sends messages to recipients in the `recipients.json` file
 
 ## Run in Development Mode
 
--- admin/app.py
+++ admin/app.py
@@ -1,23 +1,62 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
+from dotenv import load_dotenv
+from datetime import timedelta
 import json
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
+app.config["SESSION_PERMANENT"] = True
+app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=30)
+
 
 def read_recipients():
-    with open("recipients.json", "r", encoding="utf-8") as f:
+    # FIXME: refactor path to recipients.json before commit
+    with open("admin/recipients.json", "r", encoding="utf-8") as f:
         return json.load(f)
 
 
 def write_recipients(recipients):
-    with open("recipients.json", "w", encoding="utf-8") as f:
+    # FIXME: refactor path to recipients.json before commit
+    with open("admin/recipients.json", "w", encoding="utf-8") as f:
         json.dump(recipients, f, ensure_ascii=False, indent=2)
 
 
-@app.route("/", methods=["GET"])
-def home():
-    recipients = read_recipients()
-    return render_template("index.html", recipients=recipients)
+def valid_login(username, password):
+    return username == os.getenv("LOGIN") and password == os.getenv("PASSWORD")
+
+
+def do_the_login():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    error = None
+    if valid_login(username, password):
+        session["username"] = username
+        session.permanent = True
+        return redirect(url_for("home"))
+    else:
+        error = "Невірний логін або пароль"
+        return render_template("login.html", error=error)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        return do_the_login()
+    else:
+        if "username" in session:
+            return redirect(url_for("home"))
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("login"))
 
 
 @app.route("/save_recipients", methods=["POST"])
@@ -28,6 +67,19 @@ def save_recipients():
     write_recipients(data)
 
     return jsonify({"message": "Список отримувачів збережено"}), 200
+
+
+@app.route("/", methods=["GET"])
+def home():
+    if "username" not in session:
+        return redirect(url_for("login"))
+    recipients = read_recipients()
+    return render_template("index.html", recipients=recipients)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
